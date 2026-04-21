cef798e2c8eab1e025988c184606b25be9b3c951
Vkartafan <163538229+Vkartafan@users.noreply.github.com>
2026-04-21 15:01:41 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='detective',
+    version='0.1',
+    py_modules=['detective'],
+    author='Detective',
+    description='Детектив данных: следит за изменениями сумм и других метрик',
+    license='MIT',
+)
