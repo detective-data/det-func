f33ee1f78e927866013a8e7c1edc71eac61bc09b
detective-data <163538229+detective-data@users.noreply.github.com>
2026-04-21 21:16:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='detective',
-    version='0.1',
+    version='1.0',
     py_modules=['detective'],
     author='Detective',
     description='Детектив данных: следит за изменениями сумм и других метрик',
